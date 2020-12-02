27fa8217d9996e39301a06c5a0526cf6aa766fb3
Aaryatech <code@atsinfosoft.com>
2020-12-02 15:32:48 +0530

    Anmol

--- app/src/main/java/com/ats/samarthajuice/constant/Constants.java
+++ app/src/main/java/com/ats/samarthajuice/constant/Constants.java
@@ -21,7 +21,8 @@ public class Constants {
 
     // public static  String serverURL="http://192.168.2.2:8091/";
 
-    public static String serverURL = "http://192.168.2.201:8080/SamarthApi/";
+    //public static String serverURL = "http://192.168.2.201:8080/SamarthApi/";
+    public static String serverURL = "http://97.74.228.55:8080/SamarthApi/";
 
     public static String categoryImagePath = "http://97.74.228.55:8080/SamarthApi/happyfeastuploads/category/";
     public static String itemImagePath = "http://97.74.228.55:8080/SamarthApi/happyfeastuploads/item/";
